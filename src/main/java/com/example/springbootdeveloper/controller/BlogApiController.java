1e1f9da74337964ee5857fcbe65c2c4e9fe26cbd
shinaaaa <shin029abc@gmail.com>
2024-09-05 22:41:17 +0900

    feat: 블로그 글 조회 API

--- src/main/java/com/example/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/com/example/springbootdeveloper/controller/BlogApiController.java
@@ -7,10 +7,7 @@ import com.example.springbootdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +32,13 @@ public class BlogApiController {
                 .toList();
 
         return ResponseEntity.ok().body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticleById(@PathVariable Long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 }
