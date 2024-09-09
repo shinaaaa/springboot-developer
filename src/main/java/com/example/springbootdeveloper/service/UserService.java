7d204bd70f70e74b01135b5445446bb549cf3139
shinaaaa <shin029abc@gmail.com>
2024-09-09 21:25:13 +0900

    feat: 로그인, 회원가입, 로그아웃 추가

---
+++ src/main/java/com/example/springbootdeveloper/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.springbootdeveloper.service;
+
+import com.example.springbootdeveloper.domain.User;
+import com.example.springbootdeveloper.dto.AddUserRequest;
+import com.example.springbootdeveloper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
